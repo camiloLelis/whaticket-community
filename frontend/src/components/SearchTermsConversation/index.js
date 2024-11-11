9a069af717febd7b7a58b3fc919479c63848da52
camiloLelis <lafirmafuerte@gmail.com>
2024-11-11 08:43:03 -0300

    feat: cria função de busca de mensagens na api

--- frontend/src/components/SearchTermsConversation/index.js
+++ frontend/src/components/SearchTermsConversation/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 import api from "../../services/api";
 import toastError from "../../errors/toastError";
 
 const SearchMessages = () => {
+
+  const { ticketId } = useParams();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = useCallback((e) => setSearchTerm(e.target.value), []);
+
+  const handleSearch = useCallback(async (page) => {
+
+    try {
+        const { data } = await api.get(`/search/${ticketId}`, { params: { query: searchTerm, page } });
+    } catch (error) {
+        toastError(error);
+    } 
+}, [ searchTerm, ticketId]);
+
+
   return (
     <div>
       <input
         type="text"
         placeholder="Digite para buscar mensagens..."
+        value={searchTerm}
+        onChange={handleChange}
         style={{ width: "100%", padding: "8px", marginBottom: "12px" }}
       />
       {<p>Carregando...</p>}
